debc24fc18e0dfe039adeb36255c2e5a371836db
Ceyhun Ceyhun <ceyhun@ceyhun.net>
2013-12-24 08:32:19 +0000

    hidden profile and multiplayer game template update

---
+++ experiment_templates/multiplayergame/financialgame.js
@@ -0,0 +1,22 @@
+function initVars()
+{
+	variables['dynamic_instruction_count']="1";	//Number of dynamic instructions
+	variables['dynamic_instruction_image_1']="";	//dynamic instruction image 1
+	variables['dynamic_instruction_text_1']="Here are the details of the instruction";	//dynamic instruction 1 text 
+	variables['dynamic_instruction_text_1_payoff_cell']="(2,2)";	//Pay off cell for dynamic instruction text 1 which is being introduced 
+	variables['information_column_count']="1";	//Number of information columns 
+	variables['information_row1_column1_image']="nophoto.png";	//information row 1 column 1 image 
+	variables['information_row1_column1_outcome_text']="Outcome text for row 1 column 1";	//information row 1 column 1 outcome text
+	variables['information_row1_column1_payoff_cells']="(1,1) (1,2) (2,1) (2,2)";	//Payoff cells for row 1 column 1
+	variables['information_row1_column1_text']="Here are the details of the information";	//information row 1 column 1 text
+	variables['information_row_count']="1";	//Number of information rows 
+	variables['Polling_Text']="You are not currently assigned to a game. Please wait...";	//Text that is displayed if you aren't included in a round 
+	variables['Prev_Rnd_Choice_Text']="Prev. Round Choice";	//Previous round choice caption
+	variables['Prev_Rnd_Payoff_Text']="Prev. Round Payoff";	//Previous round payoff caption
+	variables['static_instruction_count']="1";	//Number of static instructions
+	variables['static_instruction_image_1']="nophoto.png";	//static instruction 1 image
+	variables['static_instruction_text_1']="Here are the details of the instruction";	//static instruction 1 text 
+	variables['Total_Payoff_Text']="Total Payoff";	//Total Payoff 
+	variables['Your_Choice_Text']="You selected ";	//Text that is displayed when a choice is selected 
+	variables['Avg_Payoff_Text']="Average Payoff";	//Average payoff text
+}
