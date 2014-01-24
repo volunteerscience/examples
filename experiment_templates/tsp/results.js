e9b78f61d1a6ec681ae4aac76970c35144c9e7d9
Jeff Hoye <jeffhoye@gmail.com>
2014-01-24 19:32:50 +0000

    update to results visualizer, get_data for TSP visualization

---
+++ experiment_templates/tsp/results.js
@@ -0,0 +1,89 @@
+function initializeVisualization() {
+  $("#startDate").datetimepicker();
+  $("#endDate").datetimepicker();
+
+//  alert("bar"); 
+  
+//  paper = Raphael("canvas", width+cityRad*2, height+cityRad*2);
+  
+  $('#update').click(function(event) {
+    updateSummary();
+    event.preventDefault();
+  });
+  
+  updateSummary();
+}
+
+function updateSummary() {
+  args = {"format":"json","type":"summary"};
+  if ($('#startDate').val()) {
+    args['start'] = $('#startDate').val();
+  }
+  if ($('#endDate').val()) {
+    args['end'] = $('#endDate').val();
+  }
+  
+  getDataHelper(function(data) {
+//    $("#results").html(JSON.stringify(data));    
+    tests = data["tests"];
+    str = "";
+    for (idx in tests) {
+      test = tests[idx];
+      str+='<div id="'+test['id']+'" class="testLink">Test:'+test['id']+' Date:'+test['date']+'</div><br/>';
+    }
+    $("#results").html(str);
+    $(".testLink").click(function() {
+      toggleData($(this));
+    });
+  }, args);  
+}
+
+function toggleData(div) {
+  id = div.attr('id');
+  nxt = div.next();
+  if (nxt.hasClass('contents')) {
+    nxt.remove();
+  } else {
+    addContents(id);
+  }
+}
+
+function addContents(id) {
+  args = {"format":"json","type":"detail","ids":id};
+  getDataHelper(function(data) {
+    var contents = $("#"+id).after('<div class="contents"></div>').next();
+    var tests = data['tests'];
+    var test = tests[0];
+    var submissions = test['submissions'];
+    var str="";
+    var map_id = 0;
+    var score = 0;
+    var canvas_id = "";
+    for (idx in submissions) {
+      var sub = submissions[idx];
+      try {        
+        // not all are solutions
+        var round = sub['round']-100;
+        var val = sub['val'];
+        var solution = val['solution'];
+        map_id = solution["map"];
+        var dist = solution["dist"];
+        var city_order = solution["#text"];
+        canvas_id = id+"_"+idx;
+        score = Math.round(dist/410);
+        contents.append('<div><div id="'+canvas_id+'"></div><p>'+round+': '+score+'</p></div>');
+        var paper = Raphael(canvas_id, (width+cityRad*2)*tFac, (height+cityRad*2)*tFac);
+        doDrawTeamMap(paper,buildMapFromIndex(map_id),city_order.split(","));
+      } catch (err) {}
+    }
+    canvas_id = id+"_opt";
+    score = Math.round(map[map_id][2][0]/410);
+    contents.append('<div><div id="'+canvas_id+'"></div><p>Opt:'+score+'</p></div>');
+    var paper = Raphael(canvas_id, (width+cityRad*2)*tFac, (height+cityRad*2)*tFac);
+    doDrawTeamMap(paper,buildMapFromIndex(map_id),map[map_id][1]);
+
+    
+//    str = JSON.stringify(data);
+  }, args); 
+}
+
