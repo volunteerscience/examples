f8c5efea1a13e6f7b809caad0b5054343983ae10
Jeff Hoye <jeffhoye@gmail.com>
2015-07-13 15:12:22 +0000

    updated csv function

--- experiment_templates/scudhunt/csv_results.py
+++ experiment_templates/scudhunt/csv_results.py
@@ -4,7 +4,7 @@ last_actual_round=506
 
 def build_csv(exp, tests, part_data=True, section_data=False, summary=False, variables=[], awards=[], scores=[]):
   from soclab.lazer.views import xml_data
-  xml_string = xml_data(exp, tests, part_data=part_data, summary=summary, variables=variables, awards=awards, scores=scores).getvalue()
+  xml_string = xml_data(exp, tests, part_data=part_data, summary=summary, variables=variables, matrices=matrices, awards=awards, scores=scores).getvalue()
   return build_csv_from_xml(xml_string)
 
 def build_csv_from_xml(xml_string):
